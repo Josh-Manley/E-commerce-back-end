29ad5724971cecdf1c522930df9dc5f49afaeb47
Josh-Manley <joshmanley45@gmail.com>
2024-03-22 18:18:10 -0600

    updated server js to sync sequelize, and fixed bugs in routes

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -49,7 +49,7 @@ router.post('/', (req, res) => {
     product_name: req.body.product_name,
     price: req.body.price,
     stock: req.body.stock,
-    tagIds: req.body.tag_id,
+    category_id: req.body.category_id,
   })
     .then(product => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -8,7 +8,7 @@ router.get('/', async (req, res) => {
   try {
     const tagData = await Tag.findAll({
       // be sure to include its associated Product data
-      include: [{ model: Product }, { model: ProductTag }],
+      include: [{ model: Product }],
     });
     res.status(200).json(tagData);
   } catch (err) {
@@ -20,9 +20,9 @@ router.get('/', async (req, res) => {
 router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   try {
-    const tagData = await Tag.findByPK(req.params.id, {
+    const tagData = await Tag.findByPk(req.params.id, {
       // be sure to include its associated Product data
-      include: [{ model: Product }, { model: ProductTag }],
+      include: [{ model: Product }],
     });
     if(!tagData) {
       res.status(404).json({ message: 'No tag found with that id'});
